0913d4c4923ae7cc85b4b9c1ac5c6d9e5290fb6d
matteostromieri <mstromieri@LAPTOP-O4J53KV1>
2024-09-29 10:19:52 +0200

    added the code to store informations to compute the bandwidth of a real edge

--- ComputeRings.py
+++ ComputeRings.py
@@ -16,7 +16,7 @@ Input:
 """
 def computeRings(h, threshold, K, G, d):
     q = DEQ.DoubleEndedQueue() # this is a double ended priority queue 
-    pd = dict() # key = virtual-path | value = data sizes
+    ed = dict() # key = real-edge | value = sum of data that goes throught this edge at the same moment
     rings = [] # this list will contain the "optimal" rings, it is the output 
     for v in K.nodes:
         q.insert(Path(v, d[v]))
@@ -35,7 +35,7 @@ def computeRings(h, threshold, K, G, d):
                     e_opt = e
             updateBandwidth(K,G,pd,e_opt,p)
             updatePriorities(q,K,G,d,d_max)
-            p.closePath(e_opt)
+            p.closePath(e_opt, ed)
             rings.append(p)
         p = q.getMin()
         for w in q.sorted_list:
@@ -76,23 +76,24 @@ def computeRings(h, threshold, K, G, d):
 
 def updatePriorities(q,K,G,d,d_max):
     pass
-
-def computePriority():
-"""
-Execution time of the ring that we get if we close p with e
-"""
-def ringCost(h, K, G, d, p, e):
-    pass
 """
 Execution time of the path that we get if we merge p and w via edge e
 """
 def cost(h, K, G, d, p, e, w):
     pass
 
+def computePriority():
+    pass
+"""
+Execution time of closed path
+"""
+def ringCost(h, d, p, e):
+    pass
+
 class Path:
     def __init__(self, source, data):
         self.path = [] # list of (u,v,key) tuples
-        self.real_edges = dict() # real edge -> times this path is passed throught this real edge
+        self.real_edges = dict() # real edge (u,v,k) -> times this path is passed throught this real edge
         self.most_trafficked_edge = 0 # the highest amount of data that passes throught the same physiscal edge
         self.most_trafficked_edge_data = list()
         self.in_path_data = MaxHeap(MAX_SIZE)
@@ -102,42 +103,70 @@ class Path:
         self.priority = 0
     
     # add an edge (u,v) to the path
-    def mergePaths(self, path, key, marker):
+    def mergePaths(self,h, ed, path, key, marker):
+        for e in self.real_edges.keys():
+            ed[e] -= sum(self.most_trafficked_edge_data[:self.real_edges[e]])
+        for e in path.real_edges.keys():
+            ed[e] -= sum(path.most_trafficked_edge_data[:path.real_edges[e]])            
         # 1st: update the list of edges
         if marker == SS:
+            self.updateRealEdges(h, self.source, path.source, key)
             path.path.insert(0,(self.source, path.source, key))
             for edge in path.path:
                 self.path.insert(0,(edge[1], edge[0], edge[2]))
             self.source = edge[1]
         elif marker == SH:
+            self.updateRealEdges(h, self.source, path.head, key)
             self.path.insert(0,(path.head, self.source, key))
             self.path = path.path + self.path
             self.source = path.source
         elif marker == HS:
+            self.updateRealEdges(h, self.head, path.source, key)
             self.path.append(0,(self.head, path.source, key))
             self.path = self.path + path.path
             self.head = path.head
         elif marker == HH:
+            self.updateRealEdges(h, self.head, path.head, key)
             self.path.append(self.head, path.head, key)
             for edge in reversed(path.path):
                 self.path.append((edge[1], edge[0], edge[2]))
             self.head = path.source
         # 2nd: update other data structures
-        # merge dictionary of real_edges
-        helpers.mergeDictionaries(self.real_edges, path.real_edges)
+        # merge dictionary of real_edges into self.real_edges
+        self.most_trafficked_edge = helpers.mergeDictionaries(self.real_edges, path.real_edges, max(self.most_trafficked_edge, path.most_trafficked_edge))
+        # now we have the maximum amount of data that goes throught the same real edge (k), therefore we
+        # - merge the heaps
+        # - extract the k highest values
+        self.in_path_data.merge(path.in_path_data)
+        data = self.most_trafficked_edge_data + path.most_trafficked_edge_data
+        for d in data:
+            self.in_path_data.insert(d)
+        self.most_trafficked_edge_data = []
+        i = 0
+        while i < self.most_trafficked_edge:
+            self.most_trafficked_edge_data.append(self.in_path_data.extractMax())
+        # now update the ed dictionary with new values
+        for e in self.real_edges.keys():
+            ed[e] = ed.setdefault(e,0) + sum(self.most_trafficked_edge_data[:self.real_edges[e]])
 
-    
-    
-            
-           
-    
-    def closePath(self, h, key):
-        self.path.append((self.head, self.source, key))
-        self.head = self.source
-        r_edges = h[frozenset([self.head, self.source])][key]
+    def updateRealEdges(self, h, head, source, key):
+        r_edges = h[frozenset([head, source])][key][0] # r_edges = [(u,v,k1,{alpha, beta}), (v,w,k2,{alpha,beta}), ...]
         for r_e in r_edges:
-            self.real_edges[r_e] += 1
-            if self.real_edges[r_e] > self.most_trafficked_edge:
+            index = helpers.edHash(r_e)
+            self.real_edges[index] = self.real_edges.setdefault(index,0) + 1
+            if self.real_edges[index] > self.most_trafficked_edge:
                 self.most_trafficked_edge += 1
                 self.most_trafficked_edge_data.append(self.in_path_data.extractMax())
+    
+    def closePath(self, h, ed, key):
+        self.path.append((self.head, self.source, key))
+        self.head = self.source
+        r_edges = h[frozenset([self.head, self.source])][key][0]
+        for r_e in r_edges:
+            index = helpers.edHash(r_e)
+            self.real_edges[index] += 1
+            if self.real_edges[index] > self.most_trafficked_edge:
+                self.most_trafficked_edge += 1
+                self.most_trafficked_edge_data.append(self.in_path_data.extractMax())
+            ed[index] = ed.setdefault(index,0) + self.most_trafficked_edge_data[self.real_edges[index]]
     
